017613cb49efd2d6bbd77c85fbdb2522024282c8
Ziyang Huang <35543666+hzytmac@users.noreply.github.com>
2018-03-08 15:13:30 -0500

    Update 7_Reverse_Integer.cpp

--- 7_Reverse_Integer.cpp
+++ 7_Reverse_Integer.cpp
@@ -4,15 +4,15 @@ public:
         int result = 0;
         while(x)
         {
-            auto prev=result;
-            result = result * 10;
-            result = result + (x % 10);
-            if (result / 10 != prev) 
+            auto prev=result; /*store the result*/
+            result = result * 10; /*update the result*/
+            result = result + (x % 10); /*update the result,move the last number to the front*/
+            if (result / 10 != prev)  /*check overflow*/
             {
                 result = 0;
                 break;
             }
-            x /= 10;
+            x /= 10; /*take out the first number*/
         }
         return result;
     }
